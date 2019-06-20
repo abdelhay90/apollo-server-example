cd5d4af294eca50d063685e36467f57570abe9b7
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-06-20 15:56:44 +0200

    implment cursor pagination

--- server/resolvers/message.js
+++ server/resolvers/message.js
@@ -1,13 +1,43 @@
 const {ForbiddenError} = require('apollo-server');
 const {combineResolvers} = require('graphql-resolvers');
 const {isAuthenticated, isMessageOwner} = require('./authorization');
+const Sequelize = require('sequelize');
 
+const toCursorHash = string => Buffer.from(string).toString('base64');
+
+const fromCursorHash = string =>
+    Buffer.from(string, 'base64').toString('ascii');
 module.exports = {
     Query: {
-        messages: async (parent, {offset = 0, limit = 100}, {models},) => {
-            return await models.Message.findAll({
-                offset, limit
+        messages: async (parent, { cursor, limit = 100 }, { models }) => {
+            const cursorOptions = cursor
+                ? {
+                    where: {
+                        createdAt: {
+                            [Sequelize.Op.lt]: fromCursorHash(cursor),
+                        },
+                    },
+                }
+                : {};
+
+            const messages = await models.Message.findAll({
+                order: [['createdAt', 'DESC']],
+                limit: limit + 1,
+                ...cursorOptions,
             });
+
+            const hasNextPage = messages.length > limit;
+            const edges = !hasNextPage ? messages : messages.slice(0, -1);
+
+            return {
+                edges,
+                pageInfo: {
+                    hasNextPage,
+                    endCursor: toCursorHash(
+                        edges[edges.length - 1].createdAt.toString(),
+                    ),
+                },
+            };
         },
         message: async (parent, {id}, {models}) => {
             return await models.Message.findByPk(id);
